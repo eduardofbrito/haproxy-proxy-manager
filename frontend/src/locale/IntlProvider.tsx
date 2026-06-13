326fb410faa1b6a8eda73c5de3384b597f283494
Eduardo Fernandes Brito <eduardo.brito@ATI.PI.GOV.BR>
2026-06-13 13:35:46 -0300

    fix: TypeScript type cast for IntlProvider messages (record<string,string>)

--- frontend/src/locale/IntlProvider.tsx
+++ frontend/src/locale/IntlProvider.tsx
@@ -98,11 +98,11 @@ const getLocale = (short = false) => {
 
 const cache = createIntlCache();
 
-const initialMessages = loadMessages(getLocale());
+const initialMessages = loadMessages(getLocale()) as Record<string, string>;
 let intl = createIntl({ locale: getLocale(), messages: initialMessages }, cache);
 
 const changeLocale = (locale: string): void => {
-  const messages = loadMessages(locale);
+  const messages = loadMessages(locale) as Record<string, string>;
   intl = createIntl({ locale, messages }, cache);
   window.localStorage.setItem("locale", locale);
   document.documentElement.lang = locale;
